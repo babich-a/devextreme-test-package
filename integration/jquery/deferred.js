cf5848bba9cfe6806ed70f1f5a8ef4e5c9b8db9a
babich-a <alexey.babych@gmail.com>
2021-07-09 10:29:01 +0300

    146-20.2

--- integration/jquery/deferred.js
+++ integration/jquery/deferred.js
@@ -1,7 +1,7 @@
 /**
  * DevExtreme (integration/jquery/deferred.js)
- * Version: 20.2.7 (build 21147-0311)
- * Build date: Thu May 27 2021
+ * Version: 20.2.7 (build 21146-1035)
+ * Build date: Wed May 26 2021
  *
  * Copyright (c) 2012 - 2021 Developer Express Inc. ALL RIGHTS RESERVED
  * Read about DevExtreme licensing here: https://js.devexpress.com/Licensing/
